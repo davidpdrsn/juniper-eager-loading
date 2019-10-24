f2d1b2984421dd098e7f0b306de046a44ed34ab4
David Pedersen <david.pdrsn@gmail.com>
2019-10-24 19:53:51 +0200

    Add macro-less HasMany example

---
+++ examples/has_many_no_macros.rs
@@ -0,0 +1,227 @@
+#![allow(unused_variables, unused_imports, dead_code)]
+
+#[macro_use]
+extern crate diesel;
+
+use juniper::{Executor, FieldResult};
+use juniper_eager_loading::{prelude::*, EagerLoading, HasMany, LoadChildrenOutput, LoadFrom};
+use juniper_from_schema::graphql_schema;
+use std::error::Error;
+
+// the examples all use Diesel, but this library is data store agnostic
+use diesel::prelude::*;
+
+graphql_schema! {
+    schema {
+      query: Query
+    }
+
+    type Query {
+      countries: [Country!]! @juniper(ownership: "owned")
+    }
+
+    type User {
+        id: Int!
+    }
+
+    type Country {
+        id: Int!
+        users: [User!]!
+    }
+}
+
+mod db_schema {
+    table! {
+        users {
+            id -> Integer,
+            country_id -> Integer,
+        }
+    }
+
+    table! {
+        countries {
+            id -> Integer,
+        }
+    }
+}
+
+mod models {
+    use diesel::prelude::*;
+
+    #[derive(Clone, Debug, Queryable)]
+    pub struct User {
+        pub id: i32,
+        pub country_id: i32,
+    }
+
+    #[derive(Clone, Debug, Queryable)]
+    pub struct Country {
+        pub id: i32,
+    }
+
+    impl juniper_eager_loading::LoadFrom<Country> for User {
+        type Error = diesel::result::Error;
+        type Connection = PgConnection;
+
+        fn load(
+            countries: &[Country],
+            _field_args: &(),
+            db: &Self::Connection,
+        ) -> Result<Vec<Self>, Self::Error> {
+            use crate::db_schema::users::dsl::*;
+            use diesel::pg::expression::dsl::any;
+
+            let country_ids = countries
+                .iter()
+                .map(|country| country.id)
+                .collect::<Vec<_>>();
+
+            users
+                .filter(country_id.eq(any(country_ids)))
+                .load::<User>(db)
+        }
+    }
+}
+
+pub struct Query;
+
+impl QueryFields for Query {
+    fn field_countries(
+        &self,
+        executor: &Executor<'_, Context>,
+        trail: &QueryTrail<'_, Country, Walked>,
+    ) -> FieldResult<Vec<Country>> {
+        let db = &executor.context().db;
+        let country_models = db_schema::countries::table.load::<models::Country>(db)?;
+        let mut country = Country::from_db_models(&country_models);
+        Country::eager_load_all_children_for_each(&mut country, &country_models, db, trail)?;
+
+        Ok(country)
+    }
+}
+
+pub struct Context {
+    db: PgConnection,
+}
+
+impl juniper::Context for Context {}
+
+#[derive(Clone)]
+pub struct User {
+    user: models::User,
+}
+
+impl UserFields for User {
+    fn field_id(&self, executor: &Executor<'_, Context>) -> FieldResult<&i32> {
+        Ok(&self.user.id)
+    }
+}
+
+#[derive(Clone)]
+pub struct Country {
+    country: models::Country,
+    users: HasMany<User>,
+}
+
+impl CountryFields for Country {
+    fn field_id(&self, executor: &Executor<'_, Context>) -> FieldResult<&i32> {
+        Ok(&self.country.id)
+    }
+
+    fn field_users(
+        &self,
+        executor: &Executor<'_, Context>,
+        trail: &QueryTrail<'_, User, Walked>,
+    ) -> FieldResult<&Vec<User>> {
+        self.users.try_unwrap().map_err(From::from)
+    }
+}
+
+impl GraphqlNodeForModel for User {
+    type Model = models::User;
+    type Id = i32;
+    type Connection = PgConnection;
+    type Error = diesel::result::Error;
+
+    fn new_from_model(model: &Self::Model) -> Self {
+        Self {
+            user: model.clone(),
+        }
+    }
+}
+
+impl EagerLoadAllChildren for User {
+    fn eager_load_all_children_for_each(
+        nodes: &mut [Self],
+        models: &[Self::Model],
+        db: &Self::Connection,
+        trail: &QueryTrail<'_, Self, Walked>,
+    ) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+impl GraphqlNodeForModel for Country {
+    type Model = models::Country;
+    type Id = i32;
+    type Connection = PgConnection;
+    type Error = diesel::result::Error;
+
+    fn new_from_model(model: &Self::Model) -> Self {
+        Self {
+            country: model.clone(),
+            users: Default::default(),
+        }
+    }
+}
+
+impl EagerLoadAllChildren for Country {
+    fn eager_load_all_children_for_each(
+        nodes: &mut [Self],
+        models: &[Self::Model],
+        db: &Self::Connection,
+        trail: &QueryTrail<'_, Self, Walked>,
+    ) -> Result<(), Self::Error> {
+        if let Some(child_trail) = trail.users().walk() {
+            let field_args = trail.users_args();
+
+            EagerLoadChildrenOfType::<
+                User,
+                EagerLoadingContextCountryForUsers,
+                _,
+            >::eager_load_children(nodes, models, db, &child_trail, &field_args)?;
+        }
+
+        Ok(())
+    }
+}
+
+struct EagerLoadingContextCountryForUsers;
+
+impl<'a> EagerLoadChildrenOfType<'a, User, EagerLoadingContextCountryForUsers, ()> for Country {
+    type FieldArguments = ();
+
+    fn load_children(
+        models: &[Self::Model],
+        field_args: &Self::FieldArguments,
+        db: &Self::Connection,
+    ) -> Result<LoadChildrenOutput<models::User, ()>, Self::Error> {
+        let child_models: Vec<models::User> = LoadFrom::load(&models, field_args, db)?;
+        Ok(LoadChildrenOutput::ChildModels(child_models))
+    }
+
+    fn is_child_of(
+        node: &Self,
+        child: &User,
+        _join_model: &(),
+        _field_args: &Self::FieldArguments,
+    ) -> bool {
+        node.country.id == child.user.country_id
+    }
+
+    fn association(node: &mut Self) -> &mut dyn Association<User> {
+        &mut node.users
+    }
+}
+
+fn main() {}
